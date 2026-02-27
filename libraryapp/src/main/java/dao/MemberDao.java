2c502230d00de9040a5ca38da736a5a956b06798
zsdavis1 <zsdavis1@asu.edu>
2026-02-27 00:31:40 -0700

    Create insertMember() method. Started listAllMembers() method

--- libraryapp/src/main/java/dao/MemberDao.java
+++ libraryapp/src/main/java/dao/MemberDao.java
@@ -1,4 +1,37 @@
 package dao;
 
+import db.Db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class MemberDao {
+
+    // INSERT A MEMBER
+    public void insertMember(String name, String email) throws Exception {
+
+        String sql = "INSERT INTO Member(Name, Email) " +
+                "VALUES(?,?)";
+
+        try (Connection conn = Db.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql))
+        {
+            ps.setString(1, name);
+            ps.setString(2, email);
+
+            ps.executeUpdate();
+        }
+    }
+
+    public void listAllMembers() throws Exception {
+        String sql = "SELECT * FROM Member ORDER BY Name ASC";
+
+        try (Connection conn = Db.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery())
+        {
+
+        }
+    }
 }
