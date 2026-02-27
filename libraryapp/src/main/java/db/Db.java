8fd56456ec3dbc549842674bc96fd286c0737f98
zsdavis1 <zsdavis1@asu.edu>
2026-02-27 16:54:28 -0700

    Delete target/, update pom.xml, and Db.java to support MySQL DB connection

--- libraryapp/src/main/java/db/Db.java
+++ libraryapp/src/main/java/db/Db.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 
 public class Db {
-    private static final String URL = "jdbc:postgresql://localhost:5432/librarydb_team15";
-    private static final String USER = "postgres";  //change to your username
-    private static final String PASSWORD = "Getmoney2";  //change to your password
+    private static final String URL = "jdbc:mysql://localhost:3306/deliverable_4";
+    private static final String USER = "root";  //change to your username
+    private static final String PASSWORD = "1234qwer!@#$QWER";  //change to your password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
