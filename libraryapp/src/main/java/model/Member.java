85cc619589437de6e4de368fd9abc9bfe980d98e
zsdavis1 <zsdavis1@asu.edu>
2026-02-26 23:49:52 -0700

    Create a Member object in Member.java

--- libraryapp/src/main/java/model/Member.java
+++ libraryapp/src/main/java/model/Member.java
@@ -1,4 +1,28 @@
 package model;
 
+/**
+ * This class represents a Member object in the DB
+ */
 public class Member {
+    private final int memberId;
+    private final String name;
+    private final String email;
+
+    public Member(int memberId, String name, String email) {
+        this.memberId = memberId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
